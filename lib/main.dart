87e5c38201a1e25f76b812d9fd850cd9c1dc0e91
Philipp <philipp.schulze-icking@web.de>
2025-10-21 23:51:49 +0200

    ui improved

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Deine App',
+      title: 'Tier Monitor',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0D47A1)),
         useMaterial3: true,
